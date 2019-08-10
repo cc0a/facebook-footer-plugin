7abd2de1b16da7eb8dbb188b08d597f2850f3960
cc0a <jmckeeco84@gmail.com>
2019-08-10 02:23:33 -0600

    stage 1 complete

--- facebook-footer-link.php
+++ facebook-footer-link.php
@@ -12,15 +12,18 @@ if(!defined('ABSPATH')){
 	exit;
 }
 
+// Global Options Variable
+$ffl_options = get_option('ffl_settings');
+
 // Load Scripts
-require_once(plugin_dir_path(__FILE__).'includes/facebook-footer-link-scripts.php'); // Calls in scripts file
+require_once(plugin_dir_path(__FILE__).'/includes/facebook-footer-link-scripts.php'); // Calls in scripts file
 
 // Load Content
-require_once(plugin_dir_path(__FILE__).'includes/facebook-footer-link-content.php'); // Calls in content file
+require_once(plugin_dir_path(__FILE__).'/includes/facebook-footer-link-content.php'); // Calls in content file
 
 if(is_admin()) {
 // Load Settings
-	require_once( plugin_dir_path( __FILE__ ) . 'includes/facebook-footer-link-settings.php' ); // Only included on the admin side
+	require_once( plugin_dir_path( __FILE__ ) . '/includes/facebook-footer-link-settings.php' ); // Only included on the admin side
 }
 
 
--- includes/facebook-footer-link-content.php
+++ includes/facebook-footer-link-content.php
@@ -6,7 +6,20 @@ function ffl_add_footer($content){
 	$footer_output .= '<span class="dashicons dashicons-facebook"></span> Find me on <a target="blank" href="http://www.facebook.com"/>Facebook</a>';
 	$footer_output .= '</div>';
 
-	return $content . $footer_output;
+	global $ffl_options;
+
+	if($ffl_options['show_in_feed']) {
+		if(is_single() || is_home() && $ffl_options['enable']){ // If single page -or- home page + turned on, display footer link
+			return $content . $footer_output;
+		}
+	} else {
+		if(is_single() && $ffl_options['enable']){ // else display on single post if option enabled
+			return $content . $footer_output;
+		}
+	}
+
+	return $content;
+
 }
 
 add_filter('the_content', 'ffl_add_footer'); // Changing content that's already there
