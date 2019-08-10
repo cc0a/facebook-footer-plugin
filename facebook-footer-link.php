39fce90e5356e1773b8d19c41b28b097822d4e86
cc0a <jmckeeco84@gmail.com>
2019-08-09 23:45:34 -0600

    first commit

---
+++ facebook-footer-link.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * Plugin Name: Facebook Footer Link
+ * Description: Adds a Facebook Profile Link to the End of Posts
+ * Version: 1.0
+ * Author: James McKee
+ */
+
+// Exit if Accessed Directly
+if(!defined('ABSPATH')){
+	exit;
+}
+
+// Load Scripts
+require_once(plugin_dir_path(__FILE__).'/includes/facebook-footer-link-scripts.php');
+
+
+
